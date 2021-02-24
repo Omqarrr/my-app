9e3dc0388aac2030a92f96a16f0aaf3c4cbef419
Omqarrr <61823711+Omqarrr@users.noreply.github.com>
2021-02-24 10:09:25 +0530

    version4 deployed
    version4 deployed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! new chnages bought to test mvn v3.0" );
+        System.out.println( "Hello World! new chnages bought to test mvn v4.0" );
     }
 }
