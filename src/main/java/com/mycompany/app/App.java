bd1d4cae9fbabbf94865f10ea35ec652c1eab9d2
Omqarrr <61823711+Omqarrr@users.noreply.github.com>
2021-02-24 10:00:59 +0530

    added SRC folder

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! new chnages bought to test mvn v3.0" );
+    }
+}
